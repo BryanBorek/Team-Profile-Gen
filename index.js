e8a886d28d955a3cec2e04535534cdb219178997
Bryan Borek <bryantborek@gmail.com>
2022-09-14 15:17:16 -0500

    wrote the prompts for the other employee options and restructured functions in indexjs a bit

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const Employee = require('./lib/Employee');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+
+function addManager() {
+    inquirer //ask for name, id, email, office
+        .prompt([
+            {
+                type: 'input',
+                name: 'Please enter your name.',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is your id number?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your email?',
+            },
+            {
+                type: 'input',
+                name: 'office',
+                message: 'What is your office number?',
+            },
+        ])
+        .then((userInput) => {
+            let manager = new Manager(userInput.name, userInput.id, userInput.email, userInput.office);
+            const managerCard = 'im a whole webpage and manager card'; //write html for the team page and manager card 
+            fs.writeFile('./dist/newTeam.html', managerCard, err => err ? console.log(err) : addEmployee());
+        })
+}
 
 function addEmployee() {
     inquirer
@@ -11,70 +41,84 @@ function addEmployee() {
             {
                 type: 'list',
                 name: 'employeeChoice',
-                message: 'What employee would you likt to add?',
-                choices: ['Manager', 'Engineer', 'Intern', 'Exit'],
+                message: 'Would you like to add more team members?',
+                choices: ['Add Engineer', 'Add Intern', 'No'],
             },
-        ])//list member options
+        ])
         .then((choice => {
             switch (choice.employeeChoice) {
-                case 'Manager':
-                    addManager();
-                    break
-                
-                case 'Engineer':
+                case 'Add Engineer':
                     addEngineer();
                     break
 
-                case 'Intern':
+                case 'Add Intern':
                     addIntern();
                     break
-                
-                case 'Exit':
+
+                case 'No':
                     console.log('Goodbye');
             }
-        }))//switch statement
+        }))
 }
 
-function addManager() {
-    inquirer //ask for name, id, email, office
+function addEngineer() {
+    inquirer //ask for name, id, email, github
         .prompt([
             {
                 type: 'input',
-                name: 'name',
-                message: 'What is this employees name?',
+                name: 'What is their name?',
             },
             {
                 type: 'input',
                 name: 'id',
-                message: 'What is this employees id number?',
+                message: 'What is their id number?',
             },
             {
                 type: 'input',
                 name: 'email',
-                message: 'What is this employees email?',
+                message: 'What is their email?',
             },
             {
                 type: 'input',
-                name: 'office',
-                message: 'What is this employees office number?',
+                name: 'github',
+                message: 'What is their GitHub link?',
             },
         ])
-        .then((userInput) => {
-            let manager = new Manager(userInput.name, userInput.id, userInput.email, userInput.office);
-            const managerCard = 'Hello'
-            fs.appendFile('./dist/newTeam.html', managerCard, err => err ? console.log(err) : console.log('Team Updated'))
-            addEmployee();
+        .then(userInput => {
+            let engineer = new Engineer(userInput.name, userInput.id, userInput.email, userInput.github);
+            const engineerCard = ' im an engineer card'; //write html for engineer card 
+            fs.appendFile('./dist/newTeam.html', engineerCard, err => err ? console.log(err) : addEmployee());
         })
 }
-// function addEngineer() {
-//     inquirer
-//         .prompt()
-//         .then()
-// }
-// function addIntern() {
-//     inquirer
-//         .prompt()
-//         .then()
-// }
 
-addEmployee();
+function addIntern() {
+    inquirer //ask for name, id, email, school
+        .prompt([
+            {
+                type: 'input',
+                name: 'What is their name?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is their id number?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is their email?',
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: 'Where do they go to school?',
+            },
+        ])
+        .then(userInput => {
+            let intern = new Intern(userInput.name, userInput.id, userInput.email, userInput.school);
+            const internCard = ' im an intern card'; //write html for engineer card 
+            fs.appendFile('./dist/newTeam.html', internCard, err => err ? console.log(err) : addEmployee());
+        })
+}
+
+addManager();
